c62dcb05ffa077631cfc087b08c3bd56bc4ca582
Aldwin Galapon <aldwin.galapon@gmail.com>
2018-09-20 12:43:00 +0800

    GTM script added 124209202018

--- optimization.php
+++ optimization.php
@@ -36,7 +36,8 @@
 	<meta name="keywords" content="" />
 	<meta name="robots" content="noimageindex, noodp, noydir" />
 	<meta name="author" content="" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+	<!-- Google Tag Manager --> <noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-PD97MSF' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-PD97MSF');</script> <!-- End Google Tag Manager -->
 </head>
 <body>
 	<header id="main-header">
